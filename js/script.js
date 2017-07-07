f791f675c4d3fefb4af75a2f530120d8967b6a0d
Hernan Mendez <hernan21112001@gmail.com>
2017-07-07 13:43:12 -0400

    Changed stop/play gif

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ $mobileNav.hide(0);
 $('nav img').click(function(){
     $mobileNav.slideToggle();
 });
+
+var check = true;
+$('#stopGif').click(function(){
+    if(check)
+    $('#header-image').css('background-image','url("./images/stop.gif")');
+    else
+    $('#header-image').css('background-image','url("./images/js.gif")'); 
+    check = !check
+});
