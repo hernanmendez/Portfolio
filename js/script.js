49197150c41e0bd00688d57f98642ea565778b20
Hernan Mendez <hernan21112001@gmail.com>
2017-07-07 10:01:00 -0400

    Stop/continue gif

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ $mobileNav.hide(0);
 $('nav img').click(function(){
     $mobileNav.slideToggle();
 });
+
+var $gif = $('#header-image');
+$gif.mouseenter(function(){
+    $gif.css('background-image','url(./images/js.gif)')
+});
+$gif.mouseleave(function(){
+    $gif.css('background-image','url(./images/stop.gif)')
+});
