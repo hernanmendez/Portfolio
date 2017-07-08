1bc0272ed5c62a165d3cb4ff0050c51ce78250b1
Hernan Mendez <hernan21112001@gmail.com>
2017-07-08 13:12:43 -0400

    added masory layout

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ $('#stopGif').click(function(){
     $('#header-image').css('background-image','url("./images/js.gif")'); 
     check = !check
 });
+
+var $containerProject = $('#Projects .container')
+var colWidth = parseInt($containerProject.css('width'),10)/3
+
+var tops = [0,0,0]
+$($containerProject.children()).each(function(index){
+    index %= 3
+    $(this).css('left', colWidth*index)
+        .css('top',tops[index])
+
+    tops[index] += parseInt($(this).css('height'),10) + 10
+})
+
+$containerProject.css('height', Math.max(...tops))
+
+$(window).resize(function(){
+    colWidth = parseInt($containerProject.css('width'),10)/3
+    
+        var top = [0,0,0]
+        $($containerProject.children()).each(function(index){
+            index %= 3
+            $(this).css('left', colWidth*index)
+                .css('top',top[index])
+
+            top[index] += parseInt($(this).css('height'),10)
+        })
+    $containerProject.css('height', Math.max(...top))
+    
+})
