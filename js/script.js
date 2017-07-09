b9166bf806cf045b0967d8dc79ca982799c48ac6
Hernan Mendez <hernan21112001@gmail.com>
2017-07-08 22:33:26 -0400

    fixed weird rendering error

--- js/script.js
+++ js/script.js
@@ -18,16 +18,16 @@ var $containerProjectMD = $('#Projects .container.hidden-xs.hidden-sm')
 var $containerProjectSM = $('#Projects .container.hidden-xs.hidden-md')
 var $containerProjectXS = $('#Projects .container.hidden-sm.hidden-md')
 function resize(){
-var colWidthMD = parseInt($containerProjectMD.css('width'),10)/3
-var colWidthSM = parseInt($containerProjectSM.css('width'),10)/2
+var colWidthMD = parseInt($containerProjectMD.css('width'))/3
+var colWidthSM = parseInt($containerProjectSM.css('width'))/2
 
 var topsMD = [0,0,0]
 $($containerProjectMD.children()).each(function(index){
     index %= 3
     $(this).css('left', colWidthMD*index)
         .css('top',topsMD[index])
-
-    topsMD[index] += parseInt($(this).css('height'),10) + 10
+        
+    topsMD[index] += parseInt($(this).css('height')) + 10
 })
 
 $containerProjectMD.css('height', Math.max(...topsMD))
@@ -38,7 +38,7 @@ $($containerProjectSM.children()).each(function(index){
     $(this).css('left', colWidthSM*index)
         .css('top',topsSM[index])
 
-    topsSM[index] += parseInt($(this).css('height'),10) + 10
+    topsSM[index] += parseInt($(this).css('height')) + 10
 })
 
 $containerProjectSM.css('height', Math.max(...topsSM))
@@ -47,8 +47,8 @@ var topsXS = 0
 $($containerProjectXS.children()).each(function(){
     $(this).css('left', 0)
         .css('top',topsXS)
-
-    topsXS += parseInt($(this).css('height'),10) + 10
+    
+    topsXS += parseInt($(this).css('height')) + 10
 })
 
 $containerProjectXS.css('height', topsXS)
