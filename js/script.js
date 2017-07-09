5c7f1d311c4f266af620038bebbae538a9e40986
Hernan Mendez <hernan21112001@gmail.com>
2017-07-09 15:54:22 -0400

    cleaning code

--- js/script.js
+++ js/script.js
@@ -14,19 +14,17 @@ $('#stopGif').click(function(){
     $('#header-image').css('background-image','url("./images/stop.gif")');
     else
     $('#header-image').css('background-image','url("./images/js.gif")'); 
-    check = !check
+    check = !checks
 });
 
-var $containerProjectMD = $('#Projects .container.hidden-xs.hidden-sm')
-var $containerProjectSM = $('#Projects .container.hidden-xs.hidden-md')
+var $containerProjectMD = $('#Projects div.container.hidden-xs.hidden-sm')
+var $containerProjectSM = $('#Projects div.container.hidden-xs.hidden-md')
 function resize(){
-var colWidthMD = parseInt($containerProjectMD.css('width'))/3
-var colWidthSM = parseInt($containerProjectSM.css('width'))/2
 
 var topsMD = [0,0,0]
 $($containerProjectMD.children()).each(function(index){
     index %= 3
-    $(this).css('left', colWidthMD*index)
+    $(this).css('left', 100/3 *index + '%')
         .css('top',topsMD[index])
         
     topsMD[index] += parseInt($(this).css('height')) + 10
@@ -35,7 +33,7 @@ $($containerProjectMD.children()).each(function(index){
 var topsSM = [0,0]
 $($containerProjectSM.children()).each(function(index){
     index %= 2
-    $(this).css('left', colWidthSM*index)
+    $(this).css('left', 50*index  + '%')
         .css('top',topsSM[index])
 
     topsSM[index] += parseInt($(this).css('height')) + 10
